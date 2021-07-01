9214b2af21ca473e3e1013d1ca79af752e9c2926
1ucky <huangjimin@zuzuche.com>
2021-07-01 13:45:32 +0800

    升级版本；

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
-export {default as Fonts} from './font'
-export {default as Platform} from './platform'
-export {default as Units} from './units'
+export {default as Fonts} from './font/Fonts'
+export {default as Platform} from './platform/Platform'
+export {default as Units} from './units/Units'
